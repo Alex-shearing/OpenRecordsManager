70a2aa2c53727b45c14a5eeac52908353d570c89
Alex-shearing <aws40404+github@gmail.com>
2026-06-28 20:26:37 +1000

    more config

--- server-core/src/main/java/com/openrecordsmanager/config/DatabaseConfigInitializationTask.java
+++ server-core/src/main/java/com/openrecordsmanager/config/DatabaseConfigInitializationTask.java
@@ -1,8 +1,8 @@
-package com.openrecordsmanager;
+package com.openrecordsmanager.config;
 
-import com.openrecordsmanager.config.DatabaseConfigSource;
 import com.openrecordsmanager.model.repositories.DataRepository;
 import com.openrecordsmanager.resources.ComponentCatalog;
+import org.jspecify.annotations.NonNull;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.env.ConfigurableEnvironment;
@@ -10,20 +10,20 @@ import org.springframework.core.env.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AppStartupTask implements ApplicationRunner {
+public class DatabaseConfigInitializationTask implements ApplicationRunner {
 
     private final ConfigurableEnvironment environment;
     private final DataRepository repository;
     private final ComponentCatalog catalog;
 
-    public AppStartupTask(ConfigurableEnvironment environment, DataRepository repository, ComponentCatalog catalog) {
+    public DatabaseConfigInitializationTask(ConfigurableEnvironment environment, DataRepository repository, ComponentCatalog catalog) {
         this.environment = environment;
         this.repository = repository;
         this.catalog = catalog;
     }
 
     @Override
-    public void run(ApplicationArguments args) throws Exception {
+    public void run(@NonNull ApplicationArguments args) {
         for (PropertySource<?> propertySource : this.environment.getPropertySources()) {
             if (propertySource instanceof DatabaseConfigSource dbCfgSrc) {
                 dbCfgSrc.initialiseLoader(this.catalog, this.repository);
