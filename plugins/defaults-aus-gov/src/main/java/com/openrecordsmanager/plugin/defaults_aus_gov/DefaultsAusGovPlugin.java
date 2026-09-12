b4442362af19c65cfeb5e66928d06f7cbb0029fd
Alex-shearing <aws40404+github@gmail.com>
2026-09-12 17:00:13 +1000

    cleanup

--- plugins/defaults-aus-gov/src/main/java/com/openrecordsmanager/plugin/defaults_aus_gov/DefaultsAusGovPlugin.java
+++ plugins/defaults-aus-gov/src/main/java/com/openrecordsmanager/plugin/defaults_aus_gov/DefaultsAusGovPlugin.java
@@ -2,13 +2,9 @@ package com.openrecordsmanager.plugin.defaults_aus_gov;
 
 import com.openrecordsmanager.api.Plugin;
 import com.openrecordsmanager.api.RegistrationContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * This is a set of defaults for the Australian Government.
- * Templates are auto-loaded from classpath JSON under {@code list/}, {@code object_property/},
- * and {@code record_type/} (see {@link com.openrecordsmanager.template.TemplateJsonLoader}).
  * <p>
  * Sources:
  * - <a href="https://www.protectivesecurity.gov.au/system/files/2025-07/pspf-release-2025.pdf">Australian Government Protective Security Policy Framework</a>
@@ -18,7 +14,6 @@ import org.slf4j.LoggerFactory;
  * - <a href="https://www.security.tas.gov.au/protective-security/information-security/infosec-2-protecting-official-information">Tasmanian Government Protective Security Policy</a>
  */
 public class DefaultsAusGovPlugin implements Plugin {
-    private static final Logger LOGGER = LoggerFactory.getLogger(DefaultsAusGovPlugin.class);
 
     @Override
     public String getName() {
@@ -27,6 +22,5 @@ public class DefaultsAusGovPlugin implements Plugin {
 
     @Override
     public void initialise(RegistrationContext registry) {
-        LOGGER.info("Initializing plugin (JSON templates auto-loaded from classpath)...");
     }
 }
